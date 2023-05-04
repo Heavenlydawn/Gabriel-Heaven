d934c942ee08537a34198231573995411c28b64e
Heaven Gabriel <gabrielheavenlydawn@gmail.com>
2023-05-04 11:18:41 +0100

    Media query modification

---
+++ script.js
@@ -0,0 +1,18 @@
+const menuBar = document.getElementById("menubar")
+const navMenu = document.querySelector(".menu")
+const navLink = document.querySelectorAll(".navLink")
+
+
+function toggle(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active'); 
+    var popup = document.getElementById('popup');
+    popup.classList.toggle('active');
+}
+
+menuBar.addEventListener("click", function navToggle(){
+        if (navMenu === ){
+            na
+        }
+})
+
